13aeb39fa1322f6c5e4da0bbff7d1f7813eb55f2
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-14 01:29:37 -0230

    working on image dialog

--- src/components/ImageViewer.js
+++ src/components/ImageViewer.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import IconButton from "@material-ui/core/IconButton";
+// import FlatButton from "@material-ui/core/FlatButton";
 import ZoomIn from "@material-ui/icons/ZoomIn";
+import { Dialog, Button } from "@material-ui/core";
 
 const styles = theme => ({
 	root: {
@@ -24,30 +26,54 @@ const styles = theme => ({
 	}
 });
 
-function ImageGridList(props) {
-	console.log("ImageGridList render");
+class ImageGridList extends Component {
+	state = {
+		open: false,
+		currentImage: ""
+	};
 
-	const { classes } = props;
-	return (
-		<div className={classes.root}>
-			<GridList cellHeight={160} className={classes.gridList} cols={3}>
-				{props.tileData.map(tile => (
-					<GridListTile key={tile.webformatURL}>
-						<img src={tile.webformatURL} alt={""} />
-						<GridListTileBar
-							subtitle={`likes: ${tile.likes} /downloads: ${tile.downloads}`}
-							title={`tags: ${tile.tags}`}
-							actionIcon={
-								<IconButton className={classes.icon}>
-									<ZoomIn />
-								</IconButton>
-							}
-						/>
-					</GridListTile>
-				))}
-			</GridList>
-		</div>
-	);
+	handleOpen(imageURL) {
+		this.setState({ open: true, currentImage: imageURL });
+		console.log("############");
+	}
+
+	handleClose = () => {
+		this.setState({ open: false });
+		console.log("############");
+	};
+
+	render() {
+		const { classes } = this.props;
+		const actions = [<Button label="Close" primary={true} onClick={this.handleClose} />];
+		return (
+			<div className={classes.root}>
+				<GridList cellHeight={160} className={classes.gridList} cols={3}>
+					{this.props.tileData.map(tile => (
+						<GridListTile key={tile.webformatURL}>
+							<img src={tile.webformatURL} alt={""} />
+							<GridListTileBar
+								subtitle={`likes: ${tile.likes} /downloads: ${tile.downloads}`}
+								title={`tags: ${tile.tags}`}
+								actionIcon={
+									<IconButton onClick={() => this.handleOpen(tile.largeImageURL)}>
+										<ZoomIn />
+									</IconButton>
+								}
+							/>
+						</GridListTile>
+					))}
+				</GridList>
+				{/*Create the dialog*/}
+				{console.log("image url is : ", this.state.currentImage)}
+				<Dialog onClose={this.handleClose} open={this.state.open}>
+					<img src={this.state.currentImage} alt="" style={{ width: "100%" }} />
+				</Dialog>
+			</div>
+		);
+	}
+}
+function showImageDialog() {
+	console.log("Show image");
 }
 
 ImageGridList.propTypes = {
